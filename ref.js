457b875f4b85c560744c0739206bd52ab0e6f75b
yansq <yan_sq@yeah.net>
2018-05-15 10:12:47 +0800

    Create ref.js

---
+++ ref.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+class App extends Component{
+  constructor(props){
+    super(props);
+  }
+  this.handleClick = this.handleClick.bind(this);
+}
+handleClick(){
+  if(this.myTextInput !== null){
+    this.myTextInput.focus();
+  }
+}
+render(){
+  return(
+    <div>
+      <input type="text" ref={(ref) => this.myTextInput = ref} />
+      <input 
+        type="button"
+        value="Focus the text input"
+        onClick={this.handleClick}
+      />
+    </div>
+  )
+}
